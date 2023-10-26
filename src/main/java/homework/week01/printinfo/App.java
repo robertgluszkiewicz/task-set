fd502cf55b6e0413676ced5cf0d7d964c53d0748
Robert Gluszkiewicz <robert.glu@icloud.com>
2023-10-27 00:36:57 +0200

    Print info User object name edited

--- src/main/java/homework/week01/printinfo/App.java
+++ src/main/java/homework/week01/printinfo/App.java
@@ -2,7 +2,7 @@ package homework.week01.printinfo;
 
 public class App {
     public static void main(String[] args) {
-        User john = new User("Alice", "Brown", 33, 1.66, true);
-        john.printInfo();
+        User alice = new User("Alice", "Brown", 33, 1.66, true);
+        alice.printInfo();
     }
 }

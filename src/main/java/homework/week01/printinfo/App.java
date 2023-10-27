0981971a74735bb781d1b1ec74983ef1337e0ae8
Robert Gluszkiewicz <robert.glu@icloud.com>
2023-10-27 12:04:24 +0200

    Print info task fixed

--- src/main/java/homework/week01/printinfo/App.java
+++
@@ -1,8 +0,0 @@
-package homework.week01.printinfo;
-
-public class App {
-    public static void main(String[] args) {
-        User alice = new User("Alice", "Brown", 33, 1.66, true);
-        alice.printInfo();
-    }
-}
